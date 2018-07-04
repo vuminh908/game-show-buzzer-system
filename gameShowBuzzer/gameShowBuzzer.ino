3671e3fa7705f1e227dde4c598e6f4633087bc23
minhv17 <minhv17@vt.edu>
2018-07-04 15:26:12 -0400

    Modified sketch to test button debouncing

--- gameShowBuzzer/gameShowBuzzer.ino
+++ gameShowBuzzer/gameShowBuzzer.ino
@@ -2,28 +2,54 @@
  * File:  gameShowBuzzer.ino
  * Name:  Minh Vu
  * Desc.: Game Show Buzzer System
- * Date:  05/26/18
- * Notes: Current sketch is a test to detect button presses and display reading of all buttons.
+ * Date:  06/28/18
+ * Notes: Current sketch is a test to sample debounced button presses.
  */
 
+// 1 reset and 4 player buttons
 const byte numBtns = 5;
 const byte btnPins[numBtns] = {2, 4, 7, 8, 12};
 byte btnVals[numBtns];
 
-void setup() {
+byte numBtnsPressed = 0;
+
+// Minimum delay between using button values
+const byte debounceTime = 25;
+long prevDebounce = 0; // Timestamp
+
+void setup()
+{
   Serial.begin(9600);
   for(int s = 0; s < numBtns; s++)
     pinMode(btnPins[s], INPUT);
 }
 
-void loop() {
-  for(int s = 0; s < numBtns; s++)
-  {
-    btnVals[s] = digitalRead(btnPins[s]);
-    Serial.print(btnVals[s]);
-    Serial.print(' ');
-  }
-  Serial.println();
-  
+void loop()
+{
+  readBtns();
   delay(10);
 }
+
+void readBtns()
+{
+  // Sample button values
+  for(int s = 0; s < numBtns; s++)
+    btnVals[s] = digitalRead(btnPins[s]);
+
+  // Use button values periodically, which is determined by debounceTime
+  if((millis() - prevDebounce) > debounceTime)
+  {
+    for(int s = 0; s < numBtns; s++)
+    {
+      if(btnVals[s] == HIGH)
+        numBtnsPressed++;
+      Serial.print(btnVals[s]);
+      Serial.print(' ');
+    }
+    Serial.println(numBtnsPressed);
+    
+    numBtnsPressed = 0;
+    prevDebounce = millis();
+  }
+}
+
