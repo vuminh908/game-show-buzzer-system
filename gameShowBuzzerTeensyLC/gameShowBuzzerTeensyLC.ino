d77c0bb120565ba49953c4def6a60cd1e9372460
vuminh908 <minhv17@vt.edu>
2021-06-12 17:45:38 -0400

    Initial commit of interrupt-driven code for Teensy LC

---
+++ gameShowBuzzerTeensyLC/gameShowBuzzerTeensyLC.ino
@@ -0,0 +1,88 @@
+/**
+ * File:  gameShowBuzzerTeensyLC.ino
+ * Name:  Minh Vu
+ * Desc.: Game Show Buzzer System
+ * Notes: This sketch is written for Teensy LC.
+ *        It may also work for Teensy 3.2 since pinouts are similar.
+ * Refs.: https://www.arduino.cc/reference/en/language/functions/external-interrupts/attachinterrupt/
+**/
+
+// Buttons 0-4 (4 player buttons and 1 reset button)
+const byte numBtns = 5;
+const byte btnPins[numBtns] = {2, 5, 7, 8, 11};
+
+// State value modified in ISRs
+// 0 = listen for button,
+// 1 = P1 button pressed, ... , 4 = P4 button pressed
+const byte LISTEN = 0;
+const byte P1 = 1;
+const byte P2 = 2;
+const byte P3 = 3;
+const byte P4 = 4;
+volatile byte currState = LISTEN;
+
+void isrRST() {
+  if (currState != LISTEN)
+  {
+    currState = LISTEN;
+    Serial.println("RST");
+  }
+}
+
+void isrP1() {
+  if (currState == LISTEN)
+  {
+    currState = P1;
+    Serial.println("P1");
+  }
+}
+
+void isrP2() {
+  if (currState == LISTEN)
+  {
+    currState = P2;
+    Serial.println("P2");
+  }
+}
+
+void isrP3() {
+  if (currState == LISTEN)
+  {
+    currState = P3;
+    Serial.println("P3");
+  }
+}
+
+void isrP4() {
+  if (currState == LISTEN)
+  {
+    currState = P4;
+    Serial.println("P4");
+  }
+}
+
+void setup() {
+  pinMode(LED_BUILTIN, OUTPUT);
+  for (byte i = 0; i < numBtns; i++)
+  {
+    pinMode(btnPins[i], INPUT);
+  }
+
+  attachInterrupt(btnPins[0], isrP1, RISING);
+  attachInterrupt(btnPins[1], isrP2, RISING);
+  attachInterrupt(btnPins[2], isrP3, RISING);
+  attachInterrupt(btnPins[3], isrP4, RISING);
+  attachInterrupt(btnPins[4], isrRST, RISING);
+  
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // TODO
+  digitalWrite(LED_BUILTIN, HIGH);
+  Serial.println(currState);
+  delay(500);
+  digitalWrite(LED_BUILTIN, LOW);
+  delay(500);
+}
